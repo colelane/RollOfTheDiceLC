28e82d1af08b12f8e743fa2aba202a489083fa7e
Lane Coleman <lane.coleman@live.com>
2020-09-19 17:43:55 -0600

    Changed quit function to detect a keypress. Fixed the odds of rolling a 12.  Equal chance of rolling any of the numbers..

--- RollOfTheDiceLC.vb
+++ RollOfTheDiceLC.vb
@@ -1,62 +1,75 @@
 ﻿Option Strict On
 Option Explicit On
 Option Compare Text
+
+'Lane Coleman
+'RCET 0265
+'Fall 2020
+'Roll Of The Dice
+'https://github.com/colelane/RollOfTheDiceLC
 Module RollOfTheDiceLC
 
     Sub Main()
         Randomize()
         Dim randomNumber As Integer
-        Dim userInput As String
         Dim data(12) As Integer
         Dim row As Integer
 
-        Console.WriteLine($"Press enter to roll the dice.")
-        userInput = Console.ReadLine()
-        Do While userInput <> "Q"
+        Console.WriteLine($"Press enter to roll the dice. Press Q to quit.")
+
+        If Console.ReadKey().Key = ConsoleKey.Q Then
+            Exit Sub
+        End If
+
+        Do
 
             For i = 1 To 1000
-                randomNumber = GetRandomNumber(1, 12)
+                randomNumber = CInt(GetRandomNumber(1, 13))
                 data(randomNumber) += 1
             Next
 
-            For i = 2 To UBound(data)
+            For i = 2 To 12
                 Console.Write("--------")
             Next
             Console.WriteLine()
 
-            For i = 2 To UBound(data)
-                Console.Write(i & "|" & vbTab)
+            For i = 2 To 12
+                Console.Write($"{i} |{vbTab}")
             Next
             Console.WriteLine()
 
-            For i = 2 To UBound(data)
+            For i = 2 To 12
                 Console.Write("--------")
             Next
             Console.WriteLine()
 
-            For i = 2 To UBound(data)
+            For i = 2 To 12
                 'Console.Write(i)
-                Console.Write(data(i) & "|" & vbTab)
+                Console.Write($"{data(i)} |{vbTab}")
             Next
 
             Console.WriteLine(vbNewLine)
-            Console.WriteLine($"Press enter to roll the dice again")
-            userInput = Console.ReadLine()
+            Console.WriteLine($"Press enter to roll the dice again. Press Q to quit")
+
+            If Console.ReadKey().Key = ConsoleKey.Q Then
+                Exit Sub
+            End If
+
 
             'clears the array
-            For row = 1 To 12
-                data(row) = Nothing
-            Next
+            Erase data
+            ReDim data(12)
+
         Loop
     End Sub
-    Function GetRandomNumber(ByVal minimum As Integer,
-                             ByVal maximum As Integer) As Integer
+    Function GetRandomNumber(ByVal minimum As Double,
+                             ByVal maximum As Double) As Double
 
-        Dim value As Single
+        Dim value As Double
 
 
         Do While value < minimum Or value > maximum
-            value = ((maximum - minimum + 1) * Rnd()) + minimum
+            value = ((maximum - minimum + 1) * Rnd()) + minimum - 1.5
         Loop
         Return CInt(value)
 
